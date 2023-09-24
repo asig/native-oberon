04cc8da18c3c6d4d9aa1600d734a6dbcabbdfd20
Andreas Signer <asigner@gmail.com>
2023-09-24 14:35:52 +0200

    update command line flags to specify an audio device; -soundhw is not available anymore.

--- 2.3.6/start.sh
+++ 2.3.6/start.sh
@@ -5,6 +5,9 @@ cd $(dirname $0)
 qemu-system-x86_64 \
     -netdev user,id=mynet0 \
     -device ne2k_pci,netdev=mynet0 \
-    -soundhw sb16 \
+    -audiodev driver=pa,id=pa1 \
+    -device sb16,audiodev=pa1 \
     -m 64 \
-    -drive file=Native\ Oberon\ 2.3.6.img,format=raw
+    -drive file=Native\ Oberon\ 2.3.6.img,format=raw \
+    -fda floppies/empty.img
+
